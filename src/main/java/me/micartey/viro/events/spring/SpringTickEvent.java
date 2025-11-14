fd84ced167dd8bc7c47a259a076d4786553bcc5a
micartey <me@micartey.dev>
2025-11-14 13:57:44 +0100

    docs: add usecase

--- src/main/java/me/micartey/viro/events/spring/SpringTickEvent.java
+++ src/main/java/me/micartey/viro/events/spring/SpringTickEvent.java
@@ -3,10 +3,14 @@ package me.micartey.viro.events.spring;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Event that is being used for animations.
+ *
+ * Will be executed every 20ms {@see me.micartey.viro.Application}
+ */
 @Getter
 @AllArgsConstructor
 public class SpringTickEvent {
 
     private final long timestamp;
-
 }
